1d0285aa22ff71b6416f795fc65fda899f9d2e5b
Chris Risley <78556958+chrisley90@users.noreply.github.com>
2025-06-10 12:32:58 -0500

    Update endofrun.sh

--- endofrun.sh
+++ endofrun.sh
@@ -16,8 +16,11 @@ DONE_DIR=$1
 ### Example commands:
 
 #cp -r "$DONE_DIR" /run/user/1001/gvfs/smb-share:server=138.26.49.223,share=globus/1
-rsync "$DONE_DIR" /run/user/1001/gvfs/smb-share:server=138.26.49.223,share=globus/1
+DST_DIR="run/user/1001/gvfs/smb-share:server=138.26.49.223,share=globus/1"
 
+rsync "$DONE_DIR" "$DST_DIR"
+
+touch "$(cut -f1 -d':' $DST_DIR)/$(basename $DONE_DIR)/.DONE:$(cut -f2 -d':' $DST_DIR)"
 # cp -r "$DONE_DIR" /mnt/NFS_SHARE
 # scp -r "$DONE_DIR" someuser@somehost:
 # curl http://someserver/api/endpoint?run=finished
